85696a9d74697681039dc5093b8667bada30dc7b
brf153 <107868772+brf153@users.noreply.github.com>
2022-09-27 17:51:58 +0530

    almost done with it

--- src/firebase.js
+++ src/firebase.js
@@ -1,15 +1,21 @@
 import firebase from "firebase";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_KEY,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyCq7EH1f4atXtv6xJ-Qp9P2Ueq_SISiFXU",
+  authDomain: "fir-project-2e8fc.firebaseapp.com",
+  projectId: "fir-project-2e8fc",
+  storageBucket: "fir-project-2e8fc.appspot.com",
+  messagingSenderId: "1002303769564",
+  appId: "1:1002303769564:web:e7d191d1286ce3671c5d19",
+  measurementId: "G-B96ETH5E72"
+};
 
-const db = firebaseApp.firestore();
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const db = initializeApp(firebaseConfig);
+/*const db = firebaseApp.firestore();*/
 
 export { db, firebaseApp, firebase};
